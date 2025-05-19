ad43d2274ccf8dea2d8399bdca2f7494b7d45bda
Zachary Milke <39819002+MilkeZa@users.noreply.github.com>
2025-05-19 16:24:34 -0400

    Refactors code and adds hardware debounced example

--- README.md
+++ README.md
@@ -18,12 +18,49 @@ This project was programmed in [MicroPython](https://docs.micropython.org/en/lat
 <!-- GETTING STARTED -->
 ## Getting Started
 
-The system can be built using off the shelf components, and the software can be uploaded using a free IDE. The behavior can also be easily modified by changing just a few lines of code in the main.py source file.
+The system can be built using off the shelf components, and the software can be uploaded using a free IDE. The behavior of the system can be easily modified by changing the main.py source file resulting in altered feed delays, indicator LED flash count, duration, etc.
+
+The indicating LED should be lit solid after the device is powered and has had time to boot, indicating that a feeding needs to occur. It is recommended the device be powered at a feeding time for this reason.
 
 
-### Hardware
+### Feeding Events
 
-While there are multiple ways to put the system together, this design used an ESP32, three jumper cables, a push button, a red LED, and a 220 ohm resistor. A Fritzing diagram can be found in the images folder.
+A *feeding event* is said to be any time the user informs the system that a feeding took place. There are two methods of telling the system a feeding event occurred:
+
+- One press of the button while indicating LED is lit: When a feeding needs to take place, the indicating LED will light up. One push of the button will signal to the system a feeding has occurred, the LED will flash three times acknowledging the event, and then turn off.
+
+- Two presses of the button while indicating LED is unlit: A feeding event can be triggered manually but double pressing the button while the indicating LED is off. Similar to the first method of signaling a feeding event, the light will flash three times and shut off.
+
+
+## Hardware
+
+All hardware used in this project are off the shelf components that anyone can purchase online or an electronics shop.
+
+
+### Component List
+
+| Name | Quantity | Purpose |
+|-|-|-|
+| Raspberry Pi Pico | 1 | Microcontroller |
+| Red LED | 1 | Indicates when a feeding needs to take place |
+| Push Button | 1 | Input telling the system when a feeding has taken place |
+| 220 ohm resistor | 1 | Limits current from breaking LED |
+| Jumper Cables | Varies | Allows for connecting components to one another |
+
+**Import Note**: Be sure to adjust the current limiting resistor sitting between the 3.3v output from the Pico and ground. A 220 ohm value is enough to keep the LED from burning out on the Red 3mm LED, but this isn't the case for diodes with different forward voltage drops.
+
+
+### Optional Hardware Changes
+
+
+#### Alternate Microcontrollers
+
+Any microcontroller containing at least one GPIO input and one output is capable of running this system. It has so far been tested on a Raspberry Pi Pico and an ESP32.
+
+
+#### Hardware Debouncing
+
+If software debouncing is something that you'd like to avoid, a simple debouncing circuit for the push button can be built using an NE555 timer IC along with a handful of resistors, capacitors, and jumper wires. For more information, search for "astable oscillator using NE555 timer" or "how to build a debounce circuit using an NE555". 
 
 
 ### Software
@@ -31,5 +68,10 @@ While there are multiple ways to put the system together, this design used an ES
 The [Thonny](https://thonny.org/) IDE was used to program the system and upload the code to the Pico.
 
 
-<!-- MARKDOWN LINKS & IMAGES -->
-[fritzing-diagram-01]: images/FishFeedReminderFritzingDiagram.png
+### Images
+
+![Software Decounced Fritzing Diagram](images/FishFeedReminderFritzingDiagram.png)
+
+![Hardware Debounced Fritzing Diagram](images/FishFeedReminderDebouncedFritzingDiagram.png)
+
+Yes, I am aware that the dou of 1k resistors wired in series adds to 2k, but I didn't have a 2k resistor installed in my Fritzing library. Additionally, the 330 and 220 ohm resistors in series along with the 2x1k (2k in real life) act as a voltage divider to bring the 5v signal output from the NE555 stepping it down below 3.3v as that is the maximum input voltage the Picos GPIO pins can handle.
---
+++ main.py
@@ -0,0 +1,234 @@
+"""
+File:			main.py
+Author:			Zachary Milke
+Description: 	This module runs at boot, controlling the light which reminds me to feed my fish.
+
+Intended Behavior
+-------------------
+- Red LED lights up 6 hours (after the most recent feeding), indicating a feeding is required
+- User performs the feeding, then clicks the push button once (1x), resetting the LED to low and restarting the timer
+- The user may click the push button twice (2x) in quick succession to reset the timer manually, as the push button
+    acts as an external interrupt for the device.
+"""
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------------------------------------------------
+
+from machine import Pin, Timer
+from micropython import const, mem_info
+from utime import sleep_ms
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# GPIO Pins
+# ---------------------------------------------------------------------------------------------------------------------
+
+PIN_LED_FEED_INDICATOR: int = const(14)		# GPIO pin tied to indicating LED
+PIN_BTN_RESET_FEED: int = const(16)			# GPIO pin tied to reset button
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Peripherals
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Red LED set HIGH (on) when a feeding needs to take place, otherwise, LOW (off).
+led_feed_indicator: Pin = Pin(PIN_LED_FEED_INDICATOR, Pin.OUT, value=0)
+
+# Click once when the indicating LED is HIGH to reset, or twice while it is LOW to manually reset the timer.
+btn_reset_feed: Pin = Pin(PIN_BTN_RESET_FEED, Pin.IN, Pin.PULL_DOWN)
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Variables
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Next two variables are used to set the duration of time between each feedings. E.g., the following values would
+#	result in a total duration of 1 hour 45 minutes from when the timer is started to when the indicator is set HIGH.
+#
+#	FEED_DELAY_SEC = 0
+#	FEED_DELAY_MIN = 45
+#	FEED_DELAY_HR = 1
+#
+# The absolute value functions act as a safety check for accidental negative values, so try to avoid removing them.
+
+FEED_DELAY_SEC: int = const(abs(0))
+FEED_DELAY_MIN: int = const(abs(0))
+FEED_DELAY_HR: int = const(abs(6))
+
+# Total feed delay takes the sum of the three values above, after converting each to milliseconds.
+total_feed_delay_ms: int = const(
+    (FEED_DELAY_SEC * 1_000) +
+    (FEED_DELAY_MIN * 60 * 1_000) +
+    FEED_DELAY_HR * 60 * 60 * 1_000)
+
+# True when a feeding is required, otherwise, False. True by default, meaning it will set indicating LED HIGH at boot.
+feed_required: bool = True
+
+# True when a feeding took place, otherwise, False. False by default.
+feed_event: bool = False
+
+# Duration of time after a button press is registered in which another press needs to occur in order for the system
+#	to register it as a double click. This value is expressed in milliseconds (1000 ms = 1 sec).
+BTN_DBL_CLK_THRESH_MS: int = const(250)
+
+# Previous state of the push button, used for software debouncing.
+btn_prev_state: bool = btn_reset_feed.value()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Software Timers
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Timer used to set the feed reminder values. Runs once and not again until reset.
+timer_feed_reminder: Timer = None
+
+# Timer used to detect a double click event. Runs once and not again until reset.
+timer_btn_dbl_clk: Timer = None
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Interrupt Service Routines
+# ---------------------------------------------------------------------------------------------------------------------
+
+def ISR_Expire_Double_Click_Timer(double_click_timer: Timer) -> None:
+    """ Expires the double click timer, thus, closing the window of time users have to register a double click
+            event.
+    
+    params
+    -----
+    double_click_timer [required, machine.Timer] : Software timer the ISR is attached to.
+    """
+    
+    # Get the global variables modified within the ISR
+    global timer_btn_dbl_clk
+
+    # Deinitialize and Nullify the double click timer
+    timer_btn_dbl_clk.deinit()
+    timer_btn_dbl_clk = None
+    
+
+def ISR_Set_Feed_Reminder(feed_reminder_timer: Timer) -> None:
+    """ Sets feed reminder variables HIGH when called by the feed reminder timer.
+
+    params
+    -----
+    feed_reminder_timer [required, machine.Timer] : Software timer the ISR is attached to.
+    """
+    
+    # Get required global variables modified within the ISR
+    global feed_required
+    global timer_feed_reminder
+    
+    # Deinitiailize and Nullify the feed reminder timer
+    timer_feed_reminder.deinit()
+    timer_feed_reminder = None
+    
+    # Set feed_required to True, indicating LED HIGH
+    feed_required = True
+    led_feed_indicator.on()
+
+
+def ISR_Handle_PB_Click(push_button: Pin) -> None:
+    """ Handle input from the push button.
+
+    params
+    -----
+    push_button [required, machine.Pin] : Push button peripheral the ISR is attached to.
+    """
+    
+    # Turn off ISR while executing to avoid more spawning
+    btn_reset_feed.irq(handler=None)
+    
+    # Grab required global variables modified within the ISR
+    global feed_event
+    global btn_prev_state
+    global timer_btn_dbl_clk
+    global timer_feed_reminder
+    
+    # Get the current state of the button
+    current_state: bool = btn_reset_feed.value()
+    
+    # Software debounce by verifying the switch just turned on and the previous state was off (state change)
+    if current_state == 1 and btn_prev_state == 0:
+        # Button state has changed to pushed (HIGH).
+        
+        # Handle button presses differently depending on whether a feeding is required or not.
+        if feed_required:
+            # Button has been pressed to indicate a feeding took place, update the feed_event boolean
+            feed_event = True
+        else:
+            # When a feeding is not required, a double click indicates a feeding took place and the feed timer should
+            #	be restarted.
+            
+            # Check if the double click timer exists, meaning the previous press opened the double click window
+            if timer_btn_dbl_clk:
+                # Double click detected, set the feed_event bool HIGH
+                timer_feed_reminder.deinit()
+                feed_event = True
+            else:
+                # Open the double click window by initializing a new timer
+                timer_btn_dbl_clk = Timer(period=BTN_DBL_CLK_THRESH_MS, mode=Timer.ONE_SHOT,
+                                          callback=ISR_Expire_Double_Click_Timer)                
+    
+    # Update the previous state of the button to the current state
+    btn_prev_state = current_state
+    
+    # Re-assign ISR to button
+    btn_reset_feed.irq(handler=ISR_Handle_PB_Click)
+
+
+# Attach the ISR_Handle_PB_Click ISR to the associated button
+btn_reset_feed.irq(handler=ISR_Handle_PB_Click)
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Static Functions
+# ---------------------------------------------------------------------------------------------------------------------
+
+def perform_feeding_event() -> None:
+    """ Performs a feeding event, resetting the feed related variables. """
+    
+    # Grab required global variables modified within the function
+    global timer_feed_reminder
+    global feed_required
+    global feed_event
+    
+    # Set the feed_required and feed_event booleans to False
+    feed_required = False
+    feed_event = False
+    
+    # Set the indicating LED LOW
+    led_feed_indicator.off()
+    
+    # Initialize a new feed reminder timer
+    timer_feed_reminder = Timer(period=total_feed_delay_ms, mode=Timer.ONE_SHOT, callback=ISR_Set_Feed_Reminder)
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Main Block
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Memory Optimizations:
+#	- Variables holding the GPIO pin values are not used passed the initialization of the peripherals, they may be
+#		deleted to save RAM.
+#	- Variables holding the FEED_DELAY_HR, FEED_DELAY_MIN values are not used after the total feed delay has been set,
+#		they may be deleted to save RAM.
+
+del PIN_LED_FEED_INDICATOR
+del PIN_BTN_RESET_FEED
+
+del FEED_DELAY_SEC
+del FEED_DELAY_MIN
+del FEED_DELAY_HR
+
+# Desired behavior is to require a feeding behavior at boot. To do this, Initialize a feed timer with a period of 1ms
+timer_feed_reminder = Timer(period=1, mode=Timer.ONE_SHOT, callback=ISR_Set_Feed_Reminder)
+
+# Main Loop (forever)
+while True:
+    # Check if a feeding is required and if a feed event took place, or if just a feed event took place.
+    if feed_event:
+        # Feeding was performed, call the perform_feeding_event function
+        perform_feeding_event()
+
